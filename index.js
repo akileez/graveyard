b42caff4f70c3ebde4825022dde4db2159b69a4c
akileez <git@keeda.net>
2015-11-03 12:47:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * graveyard <https://github.com/akileez/graveyard>
+ *
+ * Copyright © 2015 Keith Williams.
+ * Licensed under the ISC license.
+ */
+
+'use strict'
+
+module.exports = function () {
+  // do stuff
+}
